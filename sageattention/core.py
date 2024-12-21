22281e4ff672a725add16427f0f81db68cc2c343
Your Name <you@example.com>
2024-12-21 13:53:45 -0600

    Make Turning compatible. Was tested with the extension and SDXL output so far. Obviously the FP8 kernel won't work. Did not run on cuda 11.8 like the SM86 kernel did so you need cuda12.

--- sageattention/core.py
+++ sageattention/core.py
@@ -140,7 +140,7 @@ def sageattn(
     """
         
     arch = get_cuda_arch_versions()[q.device.index]
-    if arch == "sm80":
+    if arch in {"sm75", "sm80"}:
         return sageattn_qk_int8_pv_fp16_cuda(q, k, v, tensor_layout=tensor_layout, is_causal=is_causal, sm_scale=sm_scale, return_lse=return_lse, pv_accum_dtype="fp32")
     elif arch == "sm86":
         return sageattn_qk_int8_pv_fp16_triton(q, k, v, tensor_layout=tensor_layout, is_causal=is_causal, sm_scale=sm_scale, return_lse=return_lse)
