a8cc1eaa0ab8416160bc9d07a14d15fc98fa4155
woctordho <woctordho@outlook.com>
2025-03-24 20:24:46 +0800

    Use simpleindex to route torch

--- update_pyproject.py
+++ update_pyproject.py
@@ -1,4 +1,6 @@
-# Add torch version in pyproject.toml according to the environment variables
+# Add torch version in pyproject.toml
+# and set CUDA version in simpleindex.toml
+# according to the environment variables
 
 import os
 
@@ -13,4 +15,14 @@ TORCH_VERSION_NEXT = f"2.{TORCH_MINOR_VERSION}.{TORCH_PATCH_VERSION_NEXT}"
 text = text.replace('"torch"', f'"torch>={TORCH_VERSION},<{TORCH_VERSION_NEXT}"')
 
 with open("./pyproject.toml", "w") as f:
+    f.write(text)
+
+
+with open("./simpleindex.toml", "r") as f:
+    text = f.read()
+
+CUDA_MINOR_VERSION = os.getenv("CUDA_MINOR_VERSION", "6")
+text = text.replace("/cu126/", f"/cu12{CUDA_MINOR_VERSION}/")
+
+with open("./simpleindex.toml", "w") as f:
     f.write(text)
