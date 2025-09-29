1aa346a7cb4153284305d9211e6bc7098d8fbeb7
woctordho <woctordho@outlook.com>
2025-09-29 20:02:02 +0800

    Boilerplate for cibuildwheel

--- sageattention3_blackwell/setup.py
+++ sageattention3_blackwell/setup.py
@@ -152,7 +152,7 @@ class CachedWheelsCommand(bdist_wheel):
 
 setup(
     name=PACKAGE_NAME,
-    version="1.0.0",
+    version="1.0.0" + os.environ.get("SAGEATTENTION_WHEEL_VERSION_SUFFIX", ""),
     packages=find_packages(),
     description="FP4FlashAttention",
     long_description_content_type="text/markdown",
@@ -166,8 +166,4 @@ setup(
     if ext_modules
     else {"bdist_wheel": CachedWheelsCommand},
     python_requires=">=3.9",
-    install_requires=[
-        "torch",
-        "einops",
-    ],
 )
