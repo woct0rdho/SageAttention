223a7a37508d234d8502ba327fadfadbcb6ca1f3
woctordho <woctordho@outlook.com>
2025-10-07 15:18:38 +0800

    Port _qattn_sm90 to libtorch stable ABI

--- csrc/qattn/pybind_sm90.cpp
+++ csrc/qattn/pybind_sm90.cpp
@@ -15,7 +15,7 @@
  */
 
 #include <Python.h>
-#include <torch/library.h>
+#include <torch/csrc/stable/library.h>
 
 #include "attn_cuda_sm90.h"
 
@@ -38,8 +38,53 @@ extern "C" {
     }
 }
 
+void boxed_qk_int8_sv_f8_accum_f32_attn_inst_buf(
+    StableIValue* stack,
+    uint64_t num_args,
+    uint64_t num_outputs
+) {
+    auto query = to<Tensor>(stack[0]);
+    auto key = to<Tensor>(stack[1]);
+    auto value = to<Tensor>(stack[2]);
+    auto output = to<Tensor>(stack[3]);
+    auto query_scale = to<Tensor>(stack[4]);
+    auto key_scale = to<Tensor>(stack[5]);
+    auto tensor_layout = to<int64_t>(stack[6]);
+    auto is_causal = to<int64_t>(stack[7]);
+    auto qk_quant_gran = to<int64_t>(stack[8]);
+    auto sm_scale = to<double>(stack[9]);
+    auto return_lse = to<int64_t>(stack[10]);
+
+    auto lse = qk_int8_sv_f8_accum_f32_attn_inst_buf(query, key, value, output, query_scale, key_scale, tensor_layout, is_causal, qk_quant_gran, sm_scale, return_lse);
+
+    stack[0] = from(lse);
+}
+
+void boxed_qk_int8_sv_f8_accum_f32_fuse_v_scale_attn_inst_buf(
+    StableIValue* stack,
+    uint64_t num_args,
+    uint64_t num_outputs
+) {
+    auto query = to<Tensor>(stack[0]);
+    auto key = to<Tensor>(stack[1]);
+    auto value = to<Tensor>(stack[2]);
+    auto output = to<Tensor>(stack[3]);
+    auto query_scale = to<Tensor>(stack[4]);
+    auto key_scale = to<Tensor>(stack[5]);
+    auto value_scale = to<Tensor>(stack[6]);
+    auto tensor_layout = to<int64_t>(stack[7]);
+    auto is_causal = to<int64_t>(stack[8]);
+    auto qk_quant_gran = to<int64_t>(stack[9]);
+    auto sm_scale = to<double>(stack[10]);
+    auto return_lse = to<int64_t>(stack[11]);
+
+    auto lse = qk_int8_sv_f8_accum_f32_fuse_v_scale_attn_inst_buf(query, key, value, output, query_scale, key_scale, value_scale, tensor_layout, is_causal, qk_quant_gran, sm_scale, return_lse);
+
+    stack[0] = from(lse);
+}
+
 // Defines the operators
-TORCH_LIBRARY(sageattention_qattn_sm90, m) {
+STABLE_TORCH_LIBRARY(sageattention_qattn_sm90, m) {
     m.def("qk_int8_sv_f8_accum_f32_attn_inst_buf("
             "Tensor query, "
             "Tensor key, "
@@ -70,7 +115,7 @@ TORCH_LIBRARY(sageattention_qattn_sm90, m) {
 }
 
 // Registers CUDA implementations
-TORCH_LIBRARY_IMPL(sageattention_qattn_sm90, CUDA, m) {
-    m.impl("qk_int8_sv_f8_accum_f32_attn_inst_buf", &qk_int8_sv_f8_accum_f32_attn_inst_buf);
-    m.impl("qk_int8_sv_f8_accum_f32_fuse_v_scale_attn_inst_buf", &qk_int8_sv_f8_accum_f32_fuse_v_scale_attn_inst_buf);
+STABLE_TORCH_LIBRARY_IMPL(sageattention_qattn_sm90, CUDA, m) {
+    m.impl("qk_int8_sv_f8_accum_f32_attn_inst_buf", &boxed_qk_int8_sv_f8_accum_f32_attn_inst_buf);
+    m.impl("qk_int8_sv_f8_accum_f32_fuse_v_scale_attn_inst_buf", &boxed_qk_int8_sv_f8_accum_f32_fuse_v_scale_attn_inst_buf);
 }
