e97074be684e757fe9f8e98fab2742b315a8203c
woctordho <woctordho@outlook.com>
2025-10-10 17:34:35 +0800

    Fix shape and device of lse in fake impl

--- sageattention/sm80_compile.py
+++ sageattention/sm80_compile.py
@@ -29,7 +29,7 @@ def sm80_qk_fake_impl(
     if return_lse:
         lse = torch.empty((batch_size, num_qo_heads, qo_len), dtype=torch.float32, device=query.device)
     else:
-        lse = torch.empty((0))
+        lse = torch.empty((0,), dtype=torch.float32, device=query.device)
     return lse
 
 
