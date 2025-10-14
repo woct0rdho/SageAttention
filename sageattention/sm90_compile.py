b1a0e4ad1c3ca1e14febe60f05d7d5dd4ba66158
woctordho <woctordho@outlook.com>
2025-10-14 12:00:29 +0800

    Update fake impl

--- sageattention/sm90_compile.py
+++ sageattention/sm90_compile.py
@@ -4,27 +4,7 @@ from . import _qattn_sm90
 _qattn_sm90 = torch.ops.sageattention_qattn_sm90
 
 
-@torch.library.custom_op("sageattention_sm90::qk_int8_sv_f8_accum_f32_attn_inst_buf", mutates_args=("output",), device_types="cuda")
-def qk_int8_sv_f8_accum_f32_attn_inst_buf(
-    query: torch.Tensor, 
-    key: torch.Tensor, 
-    value: torch.Tensor, 
-    output: torch.Tensor, 
-    query_scale: torch.Tensor, 
-    key_scale: torch.Tensor, 
-    tensor_layout: int, 
-    is_causal: int, 
-    qk_quant_gran: int, 
-    sm_scale: float,
-    return_lse: int,
-) -> torch.Tensor:
-    return _qattn_sm90.qk_int8_sv_f8_accum_f32_attn_inst_buf(
-        query, key, value, output, query_scale, key_scale, tensor_layout,
-        is_causal, qk_quant_gran, sm_scale, return_lse
-    )
-
-
-@torch.library.register_fake("sageattention_sm90::qk_int8_sv_f8_accum_f32_attn_inst_buf")
+@torch.library.register_fake("sageattention_qattn_sm90::qk_int8_sv_f8_accum_f32_attn_inst_buf")
 def qk_int8_sv_f8_accum_f32_attn_inst_buf_fake_impl(
     query: torch.Tensor, 
     key: torch.Tensor, 
@@ -54,28 +34,7 @@ def qk_int8_sv_f8_accum_f32_attn_inst_buf_fake_impl(
     return lse
 
 
-@torch.library.custom_op("sageattention_sm90::qk_int8_sv_f8_accum_f32_fuse_v_scale_attn_inst_buf", mutates_args=("output",), device_types="cuda")
-def qk_int8_sv_f8_accum_f32_fuse_v_scale_attn_inst_buf(
-    query: torch.Tensor, 
-    key: torch.Tensor, 
-    value: torch.Tensor, 
-    output: torch.Tensor, 
-    query_scale: torch.Tensor, 
-    key_scale: torch.Tensor, 
-    value_scale: torch.Tensor,
-    tensor_layout: int, 
-    is_causal: int, 
-    qk_quant_gran: int, 
-    sm_scale: float,
-    return_lse: int,
-) -> torch.Tensor:
-    return _qattn_sm90.qk_int8_sv_f8_accum_f32_fuse_v_scale_attn_inst_buf(
-        query, key, value, output, query_scale, key_scale, value_scale,
-        tensor_layout, is_causal, qk_quant_gran, sm_scale, return_lse
-    )
-
-
-@torch.library.register_fake("sageattention_sm90::qk_int8_sv_f8_accum_f32_fuse_v_scale_attn_inst_buf")
+@torch.library.register_fake("sageattention_qattn_sm90::qk_int8_sv_f8_accum_f32_fuse_v_scale_attn_inst_buf")
 def qk_int8_sv_f8_accum_f32_fuse_v_scale_attn_inst_buf_fake_impl(
     query: torch.Tensor, 
     key: torch.Tensor, 
