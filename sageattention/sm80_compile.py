1e4b6aa796986d864de2dbb8040d8d8b2ea3cab4
woctordho <woctordho@outlook.com>
2025-12-19 15:26:50 +0800

    Fix fake impl shape when return_lse is False

--- sageattention/sm80_compile.py
+++ sageattention/sm80_compile.py
@@ -120,7 +120,7 @@ def sm80_qk_fake_impl(
     if return_lse:
         lse = torch.empty((batch_size, num_qo_heads, qo_len), dtype=torch.float32, device=query.device)
     else:
-        lse = torch.empty((0))
+        lse = torch.empty((0,))
     return lse
 
 torch.library.register_fake("sageattention::qk_int8_sv_f16_accum_f16_attn")(sm80_qk_fake_impl)
